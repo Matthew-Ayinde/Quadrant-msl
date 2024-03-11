5d3e6a66c17ebcaf070d71adf5f24550dd8ee945
Yomi Oke <okeoluwayomi9@gmail.com>
2024-03-11 01:54:09 +0100

    about us

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,13 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      spacing: {
+        "30xl": "60px",
+        "32xl": "68px",
+        "40xl": "120px",
+        "10xl": "108px",
+        "11xl": "110px",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
