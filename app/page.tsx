46a17d455638c94b4e0b886581586325cb832132
Yomi Oke <okeoluwayomi9@gmail.com>
2024-03-12 16:40:48 +0100

    update pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
-import Banner from "@/components/Banner";
-import ServiceCard from "@/components/ServiceCard";
-import HumansSection from "@/components/HumansSection";
-import CareerSection from "@/components/CareerSection";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="">
-      <Banner />
-
-      <ServiceCard />
-
-      <HumansSection />
-
-      <CareerSection />
+      <div className="w-full lg:h-screen h-[60vh] relative bg-black">
+        <video
+          className="absolute object-cover w-full h-full"
+          preload="auto"
+          loop
+          muted
+          autoPlay
+          // controls
+          controlsList="nofullscreen nodownload noremoteplayback noplaybackrate"
+          disablePictureInPicture
+          disableRemotePlayback
+          playsInline
+        >
+          <source src="/videos/qmsl-video.mp4" type="video/mp4" />
+          <Image
+            src="/banner.svg"
+            alt=""
+            fill
+            className="absolute object-cover w-auto"
+          />
+        </video>
+      </div>
     </div>
   );
 }
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,44 +2,24 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div className="w-full lg:h-screen h-[60vh] relative bg-black">
-      {/* <div className="w-full">
-        <h1 className="text-center text-3xl lg:text-[56px] font-bold text-white">
+    <div className="w-full h-full bg-custom-black lg:px-20 px-6 py-32">
+      <div className="w-full text-center">
+        <h1 className="text-3xl lg:text-[56px] font-bold text-white">
           Impact is at our core
         </h1>
-        <p className="text-center text-base lg:text-xl text-gray-500 py-5">
+
+        <p className="lg:text-xl text-custom-white mt-6">
           We are driven to offer first-rate solution to our clients
         </p>
-        <div className="relative w-full mb-10 h-[200px] lg:h-[640px]">
+        <div className="relative w-full h-[200px] lg:h-[640px] mt-14 rounded-2xl">
           <Image
             src="/banner.svg"
             alt=""
             fill
-            className="object-contain absolute w-auto"
+            className="object-cover absolute w-auto rounded-2xl"
           />
         </div>
-      </div> */}
-
-      <video
-        className="absolute object-cover w-full h-full"
-        preload="auto"
-        loop
-        muted
-        autoPlay
-        // controls
-        controlsList="nofullscreen nodownload noremoteplayback noplaybackrate"
-        disablePictureInPicture
-        disableRemotePlayback
-        playsInline
-      >
-        <source src="/videos/qmsl-video.mp4" type="video/mp4" />
-        <Image
-          src="/banner.svg"
-          alt=""
-          fill
-          className="absolute object-cover w-auto"
-        />
-      </video>
+      </div>
     </div>
   );
 };
